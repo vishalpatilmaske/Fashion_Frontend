bba138c741158bb60e1e9b3a490347dccc1091d4
vishalpatilmaske <vishpatilmaske@gmail.com>
2024-07-07 22:42:51 +0530

    create a directory structuture

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./pages/Home.jsx";
+import { Router, Routes, Route } from "react-router-dom";
+import ProductDetails from "./pages/ProductDetails.jsx";
+import ProductList from "./pages/ProductList.jsx";
+import Profile from "./pages/Profile.jsx";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "../style/globle.css";
+import Navbar from "../components/Navbar";
+
+function Home() {
+  return <Navbar />;
+}
+
+export default Home;
